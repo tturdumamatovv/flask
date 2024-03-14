2221f4d8cbaca95634e7cf46fbaabf6e86b3bbf6
tturdumamatovv <muki2022@mail.ru>
2024-03-14 21:31:34 +0600

    made new endpoints for items (delete, update), also for store (delete)

--- app.py
+++ app.py
@@ -30,6 +30,23 @@ def create_store():
     return store, 201
 
 
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.") 
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
 @app.post("/item")
 def create_item(name):
     item_data = request.get_json()
@@ -45,7 +62,7 @@ def create_item(name):
             400,
             message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload.",
         )
-    for item in item.values():
+    for item in items.values():
         if (
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
@@ -67,18 +84,33 @@ def get_all_items():
     return {"items": list(items.values())}
 
 
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.") 
-
-
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
         abort(404, message="Item not found.") 
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+        return item
+    
+    except KeyError:
+        abort(404, message="Item not found.")
