cd8a540994968bc2162e7de8e343beb5f03afd73
tturdumamatovv <muki2022@mail.ru>
2024-03-15 17:37:58 +0600

    made new way with sqlalchemy

--- db.py
+++ db.py
@@ -1,11 +1,4 @@
-stores = {}
-items = {
-    1: {
-        "name": "Chair",
-        "price": 17.99
-    },
-    2: {
-        "name": "Table",
-        "price": 180.50
-    }
-}
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
