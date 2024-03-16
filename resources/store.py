5c1af81f53f2ef0112773a2117c3bf4a04ae006c
tturdumamatovv <muki2022@mail.ru>
2024-03-16 21:51:44 +0600

    made retrieve all stores with sqlalchemy

--- resources/store.py
+++ resources/store.py
@@ -27,7 +27,7 @@ class Store(MethodView):
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
     
     @blp.arguments(StoreSchema)
     @blp.response(200, StoreSchema)
