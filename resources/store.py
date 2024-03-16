8f978166d0833f1f6ebcaa3c9ca22498bb87e53e
tturdumamatovv <muki2022@mail.ru>
2024-03-16 21:39:23 +0600

    updated file with using StoreModel

--- resources/store.py
+++ resources/store.py
@@ -15,17 +15,12 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.get_or_404(store_id)
+        raise NotImplementedError("Delating a store is not implemented.")
 
 
 @blp.route("/store")
